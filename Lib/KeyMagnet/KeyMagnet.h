06773efc88c23c8cad315d191684b42639cf22c1
Econa77 <s.f.1992.ip@gmail.com>
2016-03-09 00:10:19 +0900

    Add projects

---
+++ Lib/KeyMagnet/KeyMagnet.h
@@ -0,0 +1,19 @@
+//
+//  KeyMagnet.h
+//  KeyMagnet
+//
+//  Created by 古林俊佑 on 2016/03/09.
+//  Copyright © 2016年 Shunsuke Furubayashi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KeyMagnet.
+FOUNDATION_EXPORT double KeyMagnetVersionNumber;
+
+//! Project version string for KeyMagnet.
+FOUNDATION_EXPORT const unsigned char KeyMagnetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyMagnet/PublicHeader.h>
+
+
