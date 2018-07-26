e432f14c928ac036e4842efbc3733472e5296123
Econa77 <s.f.1992.ip@gmail.com>
2018-07-26 18:13:57 +0900

    Fix CGEvent memory leaks (#19)
    * Fix memory leak
    
    Unmanaged.passRetained to Unmanaged.passUnretained
    
    * Modify expression if to guard

--- Lib/Magnet/HotKeyCenter.swift
+++ Lib/Magnet/HotKeyCenter.swift
@@ -30,8 +30,8 @@ public final class HotKeyCenter {
 // MARK: - Register & Unregister
 public extension HotKeyCenter {
     public func register(with hotKey: HotKey) -> Bool {
-        if HotKeyCenter.shared.hotKey(hotKey.identifier) != nil { return false }
-        if hotKeys.values.contains(hotKey) { unregister(with: hotKey) }
+        guard !hotKeys.keys.contains(hotKey.identifier) else { return false }
+        guard !hotKeys.values.contains(hotKey) else { return false }
 
         if !hotKey.keyCombo.doubledModifiers {
             // Normal HotKey
@@ -59,7 +59,7 @@ public extension HotKeyCenter {
     }
     
     public func unregister(with hotKey: HotKey) {
-        if !hotKeys.values.contains(hotKey) { return }
+        guard hotKeys.values.contains(hotKey) else { return }
 
         if !hotKey.keyCombo.doubledModifiers {
             // Notmal HotKey
@@ -85,13 +85,6 @@ public extension HotKeyCenter {
 
     public func unregisterAll() {
         hotKeys.forEach { unregister(with: $1) }
-    }
-}
-
-// MARK: - HotKeys
-public extension HotKeyCenter {
-    public func hotKey(_ identifier: String) -> HotKey? {
-        return hotKeys[identifier]
     }
 }
 
@@ -167,14 +160,14 @@ private extension HotKeyCenter {
 
         // Only one modifier key
         let totalHash = commandTapped.hashValue + altTapped.hashValue + shiftTapped.hashValue + controlTapped.hashValue
-        if totalHash == 0 { return Unmanaged.passRetained(event) }
+        if totalHash == 0 { return Unmanaged.passUnretained(event) }
         if totalHash > 1 {
             multiModifiers = true
-            return Unmanaged.passRetained(event)
+            return Unmanaged.passUnretained(event)
         }
         if multiModifiers {
             multiModifiers = false
-            return Unmanaged.passRetained(event)
+            return Unmanaged.passUnretained(event)
         }
 
         if (tappedModifierKey.contains(.command) && commandTapped) ||
@@ -200,15 +193,15 @@ private extension HotKeyCenter {
         // Clean Flag
         let delay = 0.3 * Double(NSEC_PER_SEC)
         let time  = DispatchTime.now() + Double(Int64(delay)) / Double(NSEC_PER_SEC)
-        DispatchQueue.main.asyncAfter(deadline: time, execute: { [unowned self] in
-            self.tappedModifierKey = NSEvent.ModifierFlags(rawValue: 0)
+        DispatchQueue.main.asyncAfter(deadline: time, execute: { [weak self] in
+            self?.tappedModifierKey = NSEvent.ModifierFlags(rawValue: 0)
         })
 
-        return Unmanaged.passRetained(event)
+        return Unmanaged.passUnretained(event)
     }
 
     func doubleTapped(with key: Int) {
-        hotKeys.map { $0.1 }
+        hotKeys.values
             .filter { $0.keyCombo.doubledModifiers && $0.keyCombo.modifiers == key }
             .forEach { $0.invoke() }
     }
